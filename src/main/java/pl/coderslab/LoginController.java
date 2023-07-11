fe6a705347fd08a3779269ab6abb65be977e0219
Grzegorz Sroda <grzegorz_sroda@wp.pl>
2023-07-11 14:41:38 +0200

    CSS styles themed, login page view made

--- src/main/java/pl/coderslab/LoginController.java
+++ src/main/java/pl/coderslab/LoginController.java
@@ -1,9 +1,7 @@
 package pl.coderslab;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
@@ -12,12 +10,26 @@ public class LoginController {
 
     @RequestMapping("/")
 
-    public String redirectToGoogle() {
+    public String redirect() {
         return "redirect:/login";
     }
     @GetMapping("/login")
     public String showLoginPage() {
         return "login";
+    }
+    @PostMapping("/login")
+    public String login(@RequestParam("username") String username, @RequestParam("password") String password) {
+        // Implement your login logic here
+        // Validate the username and password, perform authentication, etc.
+        // You can use a service or repository to handle the login process
+
+        if (username.equals("admin") && password.equals("password")) {
+            // Successful login, redirect to the home page or desired destination
+            return "redirect:/home";
+        } else {
+            // Invalid credentials, show an error message or redirect back to the login page
+            return "redirect:/login?error";
+        }
     }
 }
 
